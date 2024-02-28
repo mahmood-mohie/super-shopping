12e6468bc931f10134d3507e090a1d14ff672d4c
maahmood-mohie <maahmood.code@gmail.com>
2024-02-28 12:13:03 +0200

    add dark-mode class according to the prefers-color-scheme and user choice and save that choice in local storage of brower

--- app/js/script.js
+++ app/js/script.js
@@ -2,5 +2,29 @@ console.log("hiiiii")
 
 
 // Dark/light Mode
+function darkMode () {
+    const switcher = document.querySelector(".top-header__switch input")
+    const useDark = window.matchMedia("(prefers-color-scheme: dark)")
+    let darkModeState = false;
+    
+    function toggleDarkMode(state){
+        document.documentElement.classList.toggle("dark-mode", state);
+        darkModeState = state;
+    }
 
+    function setDarkModeLocalStorage (state) {
+        window.localStorage.setItem("dark-mode", state )
+    }
+    // Intial settings
+    toggleDarkMode(window.localStorage.getItem("dark-mode") == true)
+    // Listen for changes in the OS settings.
+    useDark.addListener((evt)=> toggleDarkMode(evt.matches));
+    // Toggles the "dark-mode" class on click and sets localStorage state
+    switcher.addEventListener("change", ()=> {
+        darkModeState = !darkModeState;
+        toggleDarkMode(darkModeState);
+        setDarkModeLocalStorage(darkModeState)
+    })
+}
+darkMode()
 
