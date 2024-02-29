9d567d2b721eb0a57ff66b79eb4cbc00a5a28018
maahmood-mohie <maahmood.code@gmail.com>
2024-02-29 17:33:22 +0200

    optimized code of theme switcher

--- app/js/script.js
+++ app/js/script.js
@@ -1,30 +1,38 @@
-console.log("hiiiii")
-
+// [future] i use json to retrive content base on user selected language
+// content_ar.json for storing arabic version page content 
+// content_en.json for storing english version page content 
 
 // Dark/light Mode
-function darkMode () {
-    const switcher = document.querySelector(".top-header__switch input")
-    const useDark = window.matchMedia("(prefers-color-scheme: dark)")
-    let darkModeState = false;
+function theme () {
+    const switcher = document.querySelector(".Banner__themeSwitcher input");
+    const useDark = window.matchMedia("(prefers-color-scheme: dark)");
+    let darkModeState = localStorage.getItem("dark") === "true";
     
-    function toggleDarkMode(state){
+    function toggleDarkMode(state) {
         document.documentElement.classList.toggle("dark", state);
         darkModeState = state;
+        switcher.classList.toggle("checked", state)
+        // switcher.checked = state;
     }
-
-    function setDarkModeLocalStorage (state) {
-        window.localStorage.setItem("dark", state )
+    
+    function setDarkModeLocalStorage(state) {
+        localStorage.setItem("dark", state);
     }
-    // Intial settings
-    toggleDarkMode(window.localStorage.getItem("dark") == true)
+    
+    // Initial settings
+    toggleDarkMode(darkModeState);
+    
     // Listen for changes in the OS settings.
-    useDark.addListener((evt)=> toggleDarkMode(evt.matches));
+    useDark.addListener((evt) => toggleDarkMode(console.log(evt.matches)));
+    
     // Toggles the "dark-mode" class on click and sets localStorage state
-    switcher.addEventListener("change", ()=> {
+    switcher.addEventListener("change", () => {
         darkModeState = !darkModeState;
         toggleDarkMode(darkModeState);
-        setDarkModeLocalStorage(darkModeState)
-    })
+        setDarkModeLocalStorage(darkModeState);
+    });
 }
-darkMode()
+theme()
+    
+
 
